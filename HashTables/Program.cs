1729d120d190814f4ce836bfc0ba5e2c790741a9
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-22 10:03:54 +0530

    [Tirupathi] Add. Hash Table operations by using linked list

--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Common;
 
 namespace HashTables
 {
@@ -7,6 +8,15 @@ namespace HashTables
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to C# Hash Tables!");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "To");
+            hash.Add("5", "be");
+
+            Console.WriteLine("5th index : " + hash.Get("5"));
         }
     }
 }
